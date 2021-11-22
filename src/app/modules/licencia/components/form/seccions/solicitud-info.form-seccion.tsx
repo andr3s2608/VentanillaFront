a74d118c254697c75c75f7be40b12224808b7084
Sebastian Ordonez Rubiano <ingeniero.sor@gmail.com>
2021-11-22 13:46:12 -0500

    ajustes label

--- src/app/modules/licencia/components/form/seccions/solicitud-info.form-seccion.tsx
+++ src/app/modules/licencia/components/form/seccions/solicitud-info.form-seccion.tsx
@@ -97,10 +97,10 @@ export const SolicitudInfoFormSeccion: React.FC<ISolicitudInfoProps<any>> = (pro
             {/*   <Form.Item label='Representante Legal' name='solicitudRepresentanteLegal'>
               <span className='ant-form-text'>{name.toUpperCase()}</span>
             </Form.Item> */}
-            <Form.Item label='Identificación del Tramitador' name='solicitudIDTramitador'>
+            <Form.Item label='Identificación del Solicitante' name='solicitudIDTramitador'>
               <span className='ant-form-text'>{user?.numeroIdentificacion}</span>
             </Form.Item>
-            <Form.Item label='Nombres y Apellidos del Tramitador' name='solicitudIDTramitador'>
+            <Form.Item label='Nombres y Apellidos del Solicitante' name='solicitudIDTramitador'>
               <span className='ant-form-text'>{user?.fullName.toUpperCase()}</span>
             </Form.Item>
           </>
