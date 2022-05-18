b17450805e38360dab5027bbc90f0df1bf1d8263
Sebastian Ordoñez Rubiano <ingeniero.sor@gmail.com>
2022-05-18 07:49:14 -0500

    ajustes segmentacion de ambientes

--- src/app/services/Apis.service.ts
+++ src/app/services/Apis.service.ts
@@ -4,10 +4,19 @@ import { IRoles } from 'app/Models/IRoles';
 import { Menu } from 'app/Models/IMenu';
 import { IinformatioUser } from 'app/Models/IInformatioUser';
 import { Iformato } from 'app/Models/IFormato';
-import { environments } from 'environments/environments';
+
+const {
+  REACT_APP_SECURITY,
+  REACT_APP_SHARED,
+  REACT_APP_INHCREMACION,
+  REACT_APP_ENDPOINTV1,
+  REACT_APP_NOTIFICACION,
+  REACT_APP_BLOB,
+  REACT_APP_FORMATOS
+} = process.env;
 
 export class ApiService {
-  endpoint = environments.shared;
+  endpoint = REACT_APP_SHARED as string;
   private oid = '';
 
   constructor(oid$: string) {
@@ -20,15 +29,16 @@ export class ApiService {
   personaJuridica = (tipoDominio: IPersonaNatural) =>
     post<IPersonaNatural>({ endpoint: this.endpoint, url: `v2/Persona/AddPersonaJuridica`, payload: tipoDominio, id: '0' });
 
-  GetMenuUser = () => get<Menu[]>({ endpoint: environments.security, url: `Security/GetMenuByUser/${this.oid}`, id: '0' });
+  GetMenuUser = () => get<Menu[]>({ endpoint: REACT_APP_SECURITY as string, url: `Security/GetMenuByUser/${this.oid}`, id: '0' });
 
-  GetRoles = () => get<IRoles[]>({ endpoint: environments.security, url: `Security/GetRoleByIdUser/${this.oid}`, id: '0' });
+  GetRoles = () =>
+    get<IRoles[]>({ endpoint: REACT_APP_SECURITY as string, url: `Security/GetRoleByIdUser/${this.oid}`, id: '0' });
 
   AddUser = (payload: any) =>
-    post<any>({ endpoint: environments.security, url: `Security/AddUser`, payload, confirmModal: false, id: '0' });
+    post<any>({ endpoint: REACT_APP_SECURITY as string, url: `Security/AddUser`, payload, confirmModal: false, id: '0' });
 
   PostRolesUser = (payload: any) =>
-    post({ endpoint: environments.security, url: 'Security/AddUserRole', payload, confirmModal: false, id: '0' });
+    post({ endpoint: REACT_APP_SECURITY as string, url: 'Security/AddUserRole', payload, confirmModal: false, id: '0' });
 
   GetSexo = () => get<[]>({ endpoint: this.endpoint, url: 'v1/Sexo/GetSexo', id: '0' });
   GetSexoazure = () => get<[]>({ endpoint: this.endpoint, url: 'v1/Sexo/GetSexoSQL', id: '0' });
@@ -50,14 +60,14 @@ export class ApiService {
 
   getCertificado = (id: string) =>
     get<any>({
-      endpoint: environments.endpointV1,
+      endpoint: REACT_APP_ENDPOINTV1 as string,
       url: `CertificadoDefuncion/ValidateCertificadoDefuncion/${id}`,
       id: '0'
     });
 
   getMedico = (id: string) =>
     get<any>({
-      endpoint: environments.endpointV1,
+      endpoint: REACT_APP_ENDPOINTV1 as string,
       //86073058564 quemado -> ${id} dinamico
       url: `ProfesionalesSalud/GetProfesionalSaludByNumeroIdentificacion/${id}`,
       id: '0'
@@ -65,30 +75,32 @@ export class ApiService {
 
   GetAllcementerios = () =>
     get<any>({
-      endpoint: environments.endpointV1,
+      endpoint: REACT_APP_ENDPOINTV1 as string,
       url: 'Cementerio/GetAllCementerio',
       id: '0'
     });
 
   GetFunerarias = () =>
     get<any>({
-      endpoint: environments.endpointV1,
+      endpoint: REACT_APP_ENDPOINTV1 as string,
       url: 'Funeraria/GetAllFuneraria',
       id: '0'
     });
 
-  postLicencia = (payload: any) => post({ endpoint: environments.inhcremacion, url: 'Request/AddRquest', payload, id: '0' });
+  postLicencia = (payload: any) =>
+    post({ endpoint: REACT_APP_INHCREMACION as string, url: 'Request/AddRquest', payload, id: '0' });
 
-  postprueba = (payload: any) => post({ endpoint: environments.inhcremacion, url: 'Request/AddRquest', payload, id: '0' });
+  postprueba = (payload: any) => post({ endpoint: REACT_APP_INHCREMACION as string, url: 'Request/AddRquest', payload, id: '0' });
 
   AddGestion = (payload: any, id: string) =>
-    post({ endpoint: environments.inhcremacion, url: 'Request/AddGestion', payload, id });
+    post({ endpoint: REACT_APP_INHCREMACION as string, url: 'Request/AddGestion', payload, id });
 
-  putLicencia = (payload: any) => put({ endpoint: environments.inhcremacion, url: 'Request/UpdateRequest', payload, id: '0' });
+  putLicencia = (payload: any) =>
+    put({ endpoint: REACT_APP_INHCREMACION as string, url: 'Request/UpdateRequest', payload, id: '0' });
 
   uploadFiles = (payload: any) =>
     post({
-      endpoint: environments.blob,
+      endpoint: REACT_APP_BLOB as string,
       url: `Storage/AddFile`,
       payload,
       id: '0',
@@ -101,25 +113,25 @@ export class ApiService {
     });
 
   GetEstadoSolicitud = () =>
-    get<[]>({ endpoint: environments.inhcremacion, url: `Request/GetRequestByIdUser/${this.oid}`, id: '0' });
+    get<[]>({ endpoint: REACT_APP_INHCREMACION as string, url: `Request/GetRequestByIdUser/${this.oid}`, id: '0' });
 
   GetEstadoSolicitudNuevo = () =>
-    get<[]>({ endpoint: environments.inhcremacion, url: `Request/GetByIdUser/${this.oid}`, id: '0' });
+    get<[]>({ endpoint: REACT_APP_INHCREMACION as string, url: `Request/GetByIdUser/${this.oid}`, id: '0' });
 
   updatelicencia = (solicitud: string) =>
     post<any>({ endpoint: this.endpoint, url: `v2/Persona/SetApprovalInhumacionQuery/${solicitud}`, id: '1' });
 
   GetResumenSolicitud = (solicitud: string) =>
-    get<any>({ endpoint: environments.inhcremacion, url: `Request/GetResumenSolicitud/${solicitud}`, id: '0' });
+    get<any>({ endpoint: REACT_APP_INHCREMACION as string, url: `Request/GetResumenSolicitud/${solicitud}`, id: '0' });
   GetFunerariasAzure = (solicitud: string) =>
-    get<any>({ endpoint: environments.inhcremacion, url: `Request/GetFunerariabyidSolicitud/${solicitud}`, id: '0' });
+    get<any>({ endpoint: REACT_APP_INHCREMACION as string, url: `Request/GetFunerariabyidSolicitud/${solicitud}`, id: '0' });
 
   getallbyEstado = (solicitud: string) =>
-    get<[]>({ endpoint: environments.inhcremacion, url: `Request/GetRequestByIdEstado/${solicitud}`, id: '0' });
+    get<[]>({ endpoint: REACT_APP_INHCREMACION as string, url: `Request/GetRequestByIdEstado/${solicitud}`, id: '0' });
 
   AddSupportDocuments = (payload: any[]) =>
     post({
-      endpoint: environments.inhcremacion,
+      endpoint: REACT_APP_INHCREMACION as string,
       url: 'SupportDocuments/AddSupportDocuments',
       payload,
       confirmModal: false,
@@ -127,7 +139,7 @@ export class ApiService {
     });
   UpdateSupportDocuments = (payload: any[]) =>
     put({
-      endpoint: environments.inhcremacion,
+      endpoint: REACT_APP_INHCREMACION as string,
       url: 'SupportDocuments/UpdateSuport',
       payload,
       confirmModal: false,
@@ -135,42 +147,46 @@ export class ApiService {
     });
 
   getSupportDocuments = (solicitud: string) =>
-    get<any>({ endpoint: environments.inhcremacion, url: `SupportDocuments/GetAllSuportByRequestId/${solicitud}`, id: '0' });
+    get<any>({
+      endpoint: REACT_APP_INHCREMACION as string,
+      url: `SupportDocuments/GetAllSuportByRequestId/${solicitud}`,
+      id: '0'
+    });
 
   GetInformationUser = (userId: string) =>
     get<IinformatioUser>({ endpoint: this.endpoint, url: `v2/Persona/GetInfoUserById/${userId}`, id: '0' });
 
   GetAllLicencias = () =>
     get<any>({
-      endpoint: environments.inhcremacion,
+      endpoint: REACT_APP_INHCREMACION as string,
       url: 'Request/GetAllRequest',
       id: '0'
     });
 
   getCodeUser = () =>
-    get<any>({ endpoint: environments.security, url: `Security/GetCodeVentanillaByIdUser/${this.oid}`, id: '0' });
+    get<any>({ endpoint: REACT_APP_SECURITY as string, url: `Security/GetCodeVentanillaByIdUser/${this.oid}`, id: '0' });
 
   getLicencia = (solicitud: string) =>
     get<any>({
-      endpoint: environments.inhcremacion,
+      endpoint: REACT_APP_INHCREMACION as string,
       url: `Request/GetRequestById/${solicitud}`,
       id: '0'
     });
 
   //https://wa-aeu-sds-dev-tsecurity.azurewebsites.net/api/v2/Security/UpdateUser
-  putUser = (payload: any) => put<any>({ endpoint: environments.security, url: 'Security/UpdateUser', payload, id: '1' });
+  putUser = (payload: any) => put<any>({ endpoint: REACT_APP_SECURITY as string, url: 'Security/UpdateUser', payload, id: '1' });
 
   UpdataLicencia = () => {};
   GetAllTypeValidation = () =>
     get<[]>({
-      endpoint: environments.endpointV1,
+      endpoint: REACT_APP_ENDPOINTV1 as string,
       url: `Dominio/GetAllDominio/C5D41A74-09B6-4A7C-A45D-42792FCB4AC2`,
       id: '0'
     });
 
   addSeguimiento = (payload: any) => {
     return post<any>({
-      endpoint: environments.inhcremacion,
+      endpoint: REACT_APP_INHCREMACION as string,
       url: 'Seguimiento/AddSeguimiento',
       payload,
       id: '0'
@@ -179,32 +195,32 @@ export class ApiService {
 
   getUserTramite = (idTramite: string) =>
     get<any>({
-      endpoint: environments.inhcremacion,
+      endpoint: REACT_APP_INHCREMACION as string,
       url: `Seguimiento/GetSeguimientoBySolicitud/${idTramite}`,
       id: '0'
     });
 
   getCostante = (idConstante: string) =>
     get<any>({
-      endpoint: environments.inhcremacion,
+      endpoint: REACT_APP_INHCREMACION as string,
       url: `Seguimiento/GetCosntante/${idConstante}`,
       id: '0'
     });
 
   //consulta informacion adicional del fallecido
   GetInformacionFallecido = (solicitud: string) =>
-    get<any>({ endpoint: environments.inhcremacion, url: `Request/GetInfoFallecido/${solicitud}`, id: '0' });
+    get<any>({ endpoint: REACT_APP_INHCREMACION as string, url: `Request/GetInfoFallecido/${solicitud}`, id: '0' });
 
   GetSolicitud = (solicitud: string) =>
     get<any>({
-      endpoint: environments.inhcremacion,
+      endpoint: REACT_APP_INHCREMACION as string,
       url: `Request/GetAllRequestByIdSolicitud/${solicitud}`,
       id: '0'
     });
 
   sendEmail = (payload: any) => {
     return post<any>({
-      endpoint: environments.notificacion,
+      endpoint: REACT_APP_NOTIFICACION as string,
       url: 'Email/SendMail',
       payload,
       id: '1'
@@ -213,7 +229,7 @@ export class ApiService {
 
   actualizarMedico = (idMedico: string, campo: string, cambio: string) => {
     return put<any>({
-      endpoint: environments.inhcremacion,
+      endpoint: REACT_APP_INHCREMACION as string,
       url: `Request/UpdateMedico/${idMedico}/${campo}/${cambio}`,
       id: '1'
     });
@@ -221,7 +237,7 @@ export class ApiService {
 
   getFormato = (idPlantilla: string) =>
     get<Iformato>({
-      endpoint: environments.formatos,
+      endpoint: REACT_APP_FORMATOS as string,
       url: `GetFormatoByTipoPlantilla/${idPlantilla}`,
       id: '0'
     });
@@ -229,15 +245,15 @@ export class ApiService {
   /** Lista de metodos que permiten consumir los end-point que retornar los Blob
    *  de los pdf que se agregar al crear las solicitudes.
    * */
-  GetUrlPdf = (pathfull: string) => environments.blob + `Storage/GetBlob/${pathfull}`;
+  GetUrlPdf = (pathfull: string) => (REACT_APP_BLOB as string) + `Storage/GetBlob/${pathfull}`;
 
-  GeneratePDF = (idTramite: string) => `${environments.inhcremacion}GeneratePDF/GeneratePDF/${idTramite}`;
+  GeneratePDF = (idTramite: string) => `${REACT_APP_INHCREMACION as string}GeneratePDF/GeneratePDF/${idTramite}`;
 
   getLinkPDF = (idTramite: string, tramitador: string): string => {
-    return environments.inhcremacion + 'GeneratePDF/GeneratePDFPrev/' + idTramite + '/' + tramitador;
+    return (REACT_APP_INHCREMACION as string) + 'GeneratePDF/GeneratePDFPrev/' + idTramite + '/' + tramitador;
   };
 
   getLinkPDFNotificacion = (idTramite: string, tramitador: string): string => {
-    return environments.inhcremacion + 'GeneratePDF/GeneratePDF/' + idTramite + '/' + tramitador;
+    return (REACT_APP_INHCREMACION as string) + 'GeneratePDF/GeneratePDF/' + idTramite + '/' + tramitador;
   };
 }
